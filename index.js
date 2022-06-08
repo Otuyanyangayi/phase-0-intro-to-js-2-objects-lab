34851ff059e9e164b71ee872ce642d926fca52ec
Grace <gracenyangayi@gmail.com>
2022-06-08 14:19:16 +0300

    commit tree a37807a1e5a4a43aaab585105e016c3bccd3eca7

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+const employee = {
+    name: "Joy Otuya",
+    streetAdress: "church road"
+
+}
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const object1 = { ...employee }
+    object1[key] = value;
+    return object1;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
